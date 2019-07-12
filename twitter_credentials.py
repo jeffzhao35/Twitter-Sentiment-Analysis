9c9d6050da511300210b3682eda6a681ed87bdc7
Jeff <jeffzhao35@gmail.com>
2019-07-11 18:38:01 -0700

    first commit

---
+++ twitter_credentials.py
@@ -0,0 +1,6 @@
+# variables taht contain the user credentials to access Twtiter API
+
+ACCESS_TOKEN = "1031359221193949186-Dx1mllc0j7Ahg4C83eLmyhxEjMCCl9"
+ACCESS_TOKEN_SECRET = "Atd5U07n3ua11AqomtWriKAlL7syFDlkGoHFmNvrFwO1K"
+CONSUMER_KEY = "6ymxMKLmB4d4KETDryoT0Acfd"
+CONSUMER_SECRET = "57a0i9w3aHxOxaRqIdzFEa334MOzdwIQc4diOJWXLj9ZfQbvLM"
