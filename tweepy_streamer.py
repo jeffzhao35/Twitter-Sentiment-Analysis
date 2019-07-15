2a7266a5da36c5e218c7df7d5e013cecf25593f8
Jeff <jeffzhao35@gmail.com>
2019-07-15 16:29:10 -0700

    updated sentiment analysis

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -4,7 +4,13 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
+from textblob import TextBlob
+
 import twitter_credentials
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import re
 
 class TwitterClient():
 
@@ -13,6 +19,9 @@ class TwitterClient():
 		self.twitter_client = API(self.auth)
 
 		self.twitter_user = twitter_user
+
+	def get_twitter_client_api(self):
+		return self.twitter_client
 
 	def get_user_timeline_tweets(self, num_tweets):
 		tweets = []
@@ -28,7 +37,7 @@ class TwitterClient():
 
 	def get_home_timeline_tweets(self, num_tweets):
 		home_timeline_tweets = []
-		for tweet in Cursor(self.twitter_client.home_timeline id=self.twitter_user).items(num_tweets):
+		for tweet in Cursor(self.twitter_client.home_timeline, id=self.twitter_user).items(num_tweets):
 			home_timeline_tweets.append(tweet)
 		return home_timeline_tweets
 
@@ -82,13 +91,83 @@ class TwitterListener(StreamListener):
 			return False
 		print(status)
 
+class TweetAnalyzer():
+	"""
+	Functionality for analyzing and categorizing content from tweets.
+	"""
+	def clean_tweet(self, tweet):
+		return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+	def analyze_sentiment(self, tweet):
+		analysis = TextBlob(self.clean_tweet(tweet))
+
+		if analysis.sentiment.polarity > 0:
+			return 1
+		elif analysis.sentiment.polarity == 0:
+			return 0
+		else:
+			return -1
+
+	def tweets_to_data_frame(self, tweets):
+		df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['tweets'])
+		
+
+		df['id'] = np.array([tweet.id for tweet in tweets])
+		df['len'] = np.array([len(tweet.text) for tweet in tweets])
+		df['date'] = np.array([tweet.created_at for tweet in tweets])
+		df['source'] = np.array([tweet.source for tweet in tweets])
+		df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
+		df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
+
+		return df
+
 if __name__ == "__main__":
 
-	hash_tag_list = ["donald trump", "hillary clinton", "barack obama", "bernie sanders"]
-	fetched_tweets_filename = "tweets.json"
+	twitter_client = TwitterClient()
+	tweet_analyzer = TweetAnalyzer()
+	api = twitter_client.get_twitter_client_api()
 
-	twitter_client = TwitterClient('pycon')
-	print(twitter_client.get_user_timeline_tweets(1))
+	tweets = api.user_timeline(screen_name="realDonaldTrump", count=200)
+
+	# print(dir(tweets[0]))
+
+	df = tweet_analyzer.tweets_to_data_frame(tweets)
+	df['sentiment'] = np.array([tweet_analyzer.analyze_sentiment(tweet) for tweet in df['tweets']])
+
+	print(df.head(10))
+
+	# Get average length over all tweets.
+	# print(np.mean(df['len']))
+
+	# Get the number of likes for the most liked tweet
+	# print(np.max(df['likes']))
+
+	# Get the number of retweets for the most retweeted tweet.
+	# print(np.max(df['retweets']))
+
+	# print(df.head(10))
+
+
+
+	# hash_tag_list = ["donald trump", "hillary clinton", "barack obama", "bernie sanders"]
+	# fetched_tweets_filename = "tweets.json"
+
+	# twitter_client = TwitterClient('pycon')
+	# print(twitter_client.get_user_timeline_tweets(1))
 
 #	twitter_streamer = TwitterStreamer()
 #	twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
+
+
+
+	# Time Series
+	# time_likes = pd.Series(data=df['likes'].values, index=df['date'])
+	# time_likes.plot(figsize=(16, 4), label="likes", legend=True)
+
+	# time_retweets = pd.Series(data=df['retweets'].values, index=df['date'])
+	# time_retweets.plot(figsize=(16, 4), label="retweets", legend=True)
+
+	# plt.show()
+
+
+
